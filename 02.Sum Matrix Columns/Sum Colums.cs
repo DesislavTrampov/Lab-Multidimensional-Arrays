579ebf46d9c9f09abd2141f3f41f84c82f769166
exsercise workout <desislav89999@gmail.com>
2022-09-22 22:25:27 +0300

    update

--- 02.Sum Matrix Columns/Sum Colums.cs
+++ 02.Sum Matrix Columns/Sum Colums.cs
@@ -1,48 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exsercises
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+         {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            int rows = int.Parse(input[0]);
             int cols = int.Parse(input[1]);
             int[,] matrix = new int[rows, cols];
 
-            for( int row =0; row < rows; row++)
             for (int row = 0; row < rows; row++)
-            {
-                string[] rowData = Console.ReadLine().Split(" ");
-                string[] rowData = Console.ReadLine().Split(", ");
+                {
+                    string[] rowData = Console.ReadLine().Split(" ");
+                    
+                        for (int col = 0; col < cols; col++)
+                        {
+                            matrix[row, col] = int.Parse(rowData[col]);
+                        }
 
-                for(int col = 0; col<cols; col++)
+                }
+
+
+            Console.WriteLine();
+            
                 for (int col = 0; col < cols; col++)
                 {
-                    matrix[row, col] = int.Parse(rowData[col]);
+                    int sum = 0;
+
+                        for (int row = 0; row < rows; row++)
+                        {
+                            sum += matrix[row, col];
+                            if (row == matrix.GetLength(0) - 1)
+                            {
+                                Console.Write($"{matrix[row, col]}");
+                            }
+                            else
+                            {
+                                Console.Write($"{matrix[row, col]} + ");
+
+                            }
+                         
+                        }
+                    Console.WriteLine($" = {sum}");
+
+                    Console.WriteLine();
                 }
-            }
+
             
-            int sum = 0;
 
-            for (int col = 0; col < matrix.GetLength(1); col++)
-            for (int row = 0; row < rows; row++)
-            {
-                int sum = 0;
 
-                for (int row = 0; row< matrix.GetLength(0); row++)
 
-                for (int col = 0; col < cols; col++)
-                {
-                    sum += matrix[row, col];
-                    if(row == matrix.GetLength(0)-1)
-                    {
-                        Console.Write($"{matrix[row, col]}");
-                    }
-                    else
-                    {
-                        Console.Write($"{matrix[row, col]} + ");
 
-                    }
-                    //Console.Write($"{matrix[row,col]} ");
-                }
-                Console.WriteLine($" = {sum}");
 
-                Console.WriteLine();
-            }
-            
-            Console.WriteLine($"{matrix.GetLength(0)}");
-            Console.WriteLine($"{matrix.GetLength(1)}");
 
-         
+
+
+
+        }
+    }
+}
+        
